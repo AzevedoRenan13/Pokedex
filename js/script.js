8c47b1af49c935df678d231609982f89be1bc00a
Azevedo <azevedorenan13@gmail.com>
2023-08-28 23:30:32 -0300

    Criar pasta assets, separar arquivo para o consumo da poke-api,criar função para o consumo da api.

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const offset = 0
+const limit = 10
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function covertPokemomToLi(pokemon) {
+    return `
+    <li class="pokemon">
+    <span class="number"> #001 </span>
+    <span class="name">${pokemon.name}</span>
+
+    <div class="detail">
+        <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+        </ol>
+
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            alt="${pokemon.name}">
+        </div>
+    </li>`
+}
+
+
+ const pokemonList = document.getElementById('pokemonList')
+  
+ pokeApi.getPokemons().then((pokemons = []) => {
+
+       const newList =  pokemons.map((pokemon) => {
+            return pokemon.name
+        })
+
+        console.log(newList)
+
+
+        
+        const listItems = []
+
+        for (let index = 0; index < pokemons.length; index++) {
+             const pokemon = pokemons[index];
+            listItems.push(covertPokemomToLi(pokemon))
+        }
+            console.log(listItems)
+    })
+
